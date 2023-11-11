1aed2d1c21e1ca0d74a6c5603c27f82b59cc4e56
rik <ingeniero_rik@hotmail.com>
2023-11-11 19:24:52 -0300

    se actualizo shop2

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -1,8 +1,24 @@
+
 const path = require("path");
+const { json } = require("express");
+
+const fs = require("fs");
+
+
+//defino la ruta donde estan los archivos Json
+const allFunkosPath = path.join(__dirname, "../data/dataFunkos.json");
+
+
+//leo los archivos json y los guardo en variables
+const funkosJson = JSON.parse(fs.readFileSync(allFunkosPath, "utf-8"));
+
 
 const shopControllers = {
    
-    shop2:(req,res) => res.render('shop2.ejs'),
+    shop2:(req,res) =>  {
+
+        return  res.render('shop2.ejs', { allFunkos : funkosJson });
+    },
    
     cart: (req,res) => res.render('cart.ejs'),
 
