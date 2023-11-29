834a7e5847921ba8d37a4dcc19195b47aabb6006
rik <ingeniero_rik@hotmail.com>
2023-11-28 23:46:33 -0300

    se actualiza admin.ejs

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,10 +1,20 @@
 
 const path = require("path");
+const { json } = require("express");
+
+const fs = require("fs");
+
+//defino la ruta donde estan los archivos Json
+const productsPath = path.join(__dirname, "../data/listaProductos.json");
+
+
+//leo los archivos json y los guardo en variables
+const productosJson = JSON.parse(fs.readFileSync(productsPath, "utf-8"));
 
 
 const adminControllers = {
    
-    admin: (req,res) => res.render('admin.ejs'),
+    admin: (req,res) => res.render('admin.ejs', { products:productosJson }),
     create: (req,res) => res.render( 'create.ejs'),
     edit: (req,res) => res.render( 'edit.ejs')
 }
--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -22,9 +22,9 @@ const shopControllers = {
    
     cart: (req,res) => res.render('cart.ejs'),
 
-    items: (req,res) => res.render('item.ejs')
+    items: (req,res) => res.render('item.ejs'),
 
-    //edit_item: (req,res) => res.sendFile( path.join(__dirname,'../views/item/:id.html')),
+    //edit_item: (req,res) => res.render('item.ejs',{ allFunkos : funkosJson })
 }
 
 module.exports = shopControllers;
